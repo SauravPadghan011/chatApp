4167847c3f31d521407e47d366084e70afb36beb
Saurav Padghan <sauravpadghan011@gmail.com>
2021-12-07 23:37:56 +0530

    Login page and OTP page designed

--- app/src/main/java/com/amit/firstapp/chatapp/MainActivity.java
+++ app/src/main/java/com/amit/firstapp/chatapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.amit.firstapp.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText inputMobile = findViewById(R.id.input_mobile);
+        final Button buttonGetOTP = findViewById(R.id.continue_btn);
+
+        buttonGetOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(inputMobile.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Enter Mobile", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(getApplicationContext(), activity_otp.class);
+                intent.putExtra("mobile", inputMobile.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/amit/firstapp/chatapp/activity_otp.java
+++ app/src/main/java/com/amit/firstapp/chatapp/activity_otp.java
@@ -3,12 +3,124 @@ package com.amit.firstapp.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activity_otp extends AppCompatActivity {
+
+    private EditText inputOTP1, inputOTP2, inputOTP3, inputOTP4, inputOTP5, inputOTP6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+
+        inputOTP1 = findViewById(R.id.inputOTP1);
+        inputOTP2 = findViewById(R.id.inputOTP2);
+        inputOTP3 = findViewById(R.id.inputOTP3);
+        inputOTP4 = findViewById(R.id.inputOTP4);
+        inputOTP5 = findViewById(R.id.inputOTP5);
+        inputOTP6 = findViewById(R.id.inputOTP6);
+
+        setupOTPInputs();
+    }
+
+    private void setupOTPInputs() {
+        inputOTP1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputOTP2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputOTP2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputOTP3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputOTP3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputOTP4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputOTP4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputOTP5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputOTP5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().trim().isEmpty()) {
+                    inputOTP6.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
