050cc943f2c7738aa5964a9bb17e7ca816407224
Saurav Padghan <sauravpadghan011@gmail.com>
2021-12-12 00:47:01 +0530

    code comments added

--- app/src/main/java/com/amit/firstapp/chatapp/MainActivity.java
+++ app/src/main/java/com/amit/firstapp/chatapp/MainActivity.java
@@ -16,18 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText inputMobile = findViewById(R.id.input_mobile);
-        final Button buttonGetOTP = findViewById(R.id.continue_btn);
+        final EditText inputMobile = findViewById(R.id.input_mobile);                   // Input mobile number variable
+        final Button buttonGetOTP = findViewById(R.id.continue_btn);                    // Submit button variable
 
         buttonGetOTP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // If user input is empty then show pop up "Enter Mobile"
                 if(inputMobile.getText().toString().trim().isEmpty()) {
                     Toast.makeText(MainActivity.this, "Enter Mobile", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                // Start next activity after entering mobile number
                 Intent intent = new Intent(getApplicationContext(), activity_otp.class);
-                intent.putExtra("mobile", inputMobile.getText().toString());
+//                intent.putExtra("mobile", inputMobile.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/amit/firstapp/chatapp/activity_otp.java
+++ app/src/main/java/com/amit/firstapp/chatapp/activity_otp.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 public class activity_otp extends AppCompatActivity {
 
+    // Input variables for OTP
     private EditText inputOTP1, inputOTP2, inputOTP3, inputOTP4, inputOTP5, inputOTP6;
 
     @Override
@@ -17,17 +18,18 @@ public class activity_otp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
 
-        inputOTP1 = findViewById(R.id.inputOTP1);
-        inputOTP2 = findViewById(R.id.inputOTP2);
-        inputOTP3 = findViewById(R.id.inputOTP3);
-        inputOTP4 = findViewById(R.id.inputOTP4);
-        inputOTP5 = findViewById(R.id.inputOTP5);
-        inputOTP6 = findViewById(R.id.inputOTP6);
+        inputOTP1 = findViewById(R.id.inputOTP1);                               // input OTP box1
+        inputOTP2 = findViewById(R.id.inputOTP2);                               // input OTP box2
+        inputOTP3 = findViewById(R.id.inputOTP3);                               // input OTP box3
+        inputOTP4 = findViewById(R.id.inputOTP4);                               // input OTP box4
+        inputOTP5 = findViewById(R.id.inputOTP5);                               // input OTP box5
+        inputOTP6 = findViewById(R.id.inputOTP6);                               // input OTP box6
 
         setupOTPInputs();
     }
 
     private void setupOTPInputs() {
+        // Moving to the next block after entering.
         inputOTP1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -47,6 +49,7 @@ public class activity_otp extends AppCompatActivity {
             }
         });
 
+        // Moving to the next block after entering.
         inputOTP2.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -66,6 +69,7 @@ public class activity_otp extends AppCompatActivity {
             }
         });
 
+        // Moving to the next block after entering.
         inputOTP3.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -85,6 +89,7 @@ public class activity_otp extends AppCompatActivity {
             }
         });
 
+        // Moving to the next block after entering.
         inputOTP4.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -104,6 +109,7 @@ public class activity_otp extends AppCompatActivity {
             }
         });
 
+        // Moving to the next block after entering.
         inputOTP5.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
